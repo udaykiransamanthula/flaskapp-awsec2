40688933779d619551b228ebba984cfab2f82b0b
unknown <samanthulaudaykiran@gmail.com>
2024-10-26 12:30:26 +0530

    edited code second bintu  time

--- Src/app.py
+++ Src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> This is udaykiran</h1><br><p>Thank you . this is second time I hope you enjoyed it, follow for more content around Devops.</p> '
+    return '<h1> This is udaykiran</h1><br><p>Thank you . this is second time i told to bintu  I hope you enjoyed it, follow for more content around Devops.</p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
