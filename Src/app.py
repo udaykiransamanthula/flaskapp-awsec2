89953104d4aeb76640de1f8db29272cb53ff7efa
udaykiransamanthula <52404046+udaykiransamanthula@users.noreply.github.com>
2024-10-30 18:56:49 +0530

    Update app.py

--- Src/app.py
+++ Src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> This is udaykiran</h1><br><p>Thank you . this is latest after destroy.</p> '
+    return '<h1> This is udaykiran</h1><br><p>Thank you everyone. this is latest after destroy.</p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
